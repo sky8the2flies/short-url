ad1783eba1d50103104736823aea5dc9966e95a4
skylerseifert <skylerseifert@gmail.com>
2020-08-21 19:27:05 -0700

    Update mongo-url

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/shorturls', {
+mongoose.connect(`${process.env.MONGO_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
